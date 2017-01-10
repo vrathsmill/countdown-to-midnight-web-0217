0fc66f3d75033fe761d86a8363a856eab6d92058
Victoria Rathsmill <vlrathsmill@gmail.com>
2017-01-10 23:41:47 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_secs)
+    sleep(num_secs)
 end
